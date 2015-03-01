fe4f86c77306d385d396f414ebc2bd767373d333
Dave Evans <rdevans0@gmail.com>
2015-03-01 14:51:38 -0800

    Added game library
    Designed to control game state

---
+++ androtag_test/arduino_main/game.h
@@ -0,0 +1,20 @@
+
+
+#ifndef GAME_H
+#define GAME_H
+
+#define GAME_DISABLED 0
+#define GAME_WAITING 1
+#define GAME_RUNNING   2
+#define GAME_PAUSED 3 // Partial implementation, I wouldn't suggest using it
+#define GAME_ENDED 4
+
+
+extern int gid;
+void setGameStart(unsigned long time);
+void setGameEnd(unsigned long time);
+void setGameState(int state);
+void updateGame();
+
+
+#endif
